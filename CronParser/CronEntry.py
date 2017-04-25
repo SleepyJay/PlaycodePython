e4c9c02a3b5c1c1a0f32dbfb9eff2662504e936c
Jason Agosta <agostaj@d20green.com>
2017-04-24 21:03:32 -0400

    v0.7.1 cron entry

--- CronParser/CronEntry.py
+++ CronParser/CronEntry.py
@@ -1,22 +1,52 @@
 # CronEntry
 
+import collections
+
 class CronEntry(object):
+#   0       1       2        3          4         5       6
+    Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday = range(7)
 
     #
     def __init__(self, sched, cmd):
-        min, hr, dmo, mon, dow = sched.split()
+        self.minutes     = self.parseMinutes(sched.minutes)
+        self.hours       = self.parseHours(sched.hours)
+        self.month_days  = self.parseMonthDays(sched.month_days)
+        self.months      = self.parseMonths(sched.months)
+        self.weekdays    = self.parseWeekDays(sched.weekdays)
 
-        self.minute     = min  # 1
-        self.hour       = hr   # 2
-        self.day_month  = dmo  # 3
-        self.month      = mon  # 4
-        self.day_week   = dow  # 5
-        self.command    = cmd  # 6
-        
-        self.sched            = sched
+        self.command    = cmd
+        self.sched      = sched
 
+    #
+    def parseMinutes(self, min_str):
+        if min_str == '*':
+            return min_str
+        return map( (lambda x: int(x)), min_str.split(',') )
 
-        
+    #
+    def parseHours(self, hour_str):
+        if hour_str == '*':
+            return hour_str
+        return map( (lambda x: int(x)), hour_str.split(',') )
+
+    #
+    def parseMonthDays(self, day_str):
+        if day_str == '*':
+            return day_str
+        return map( (lambda x: int(x)), day_str.split(',') )
+
+    #
+    def parseMonths(self, month_str):
+        if month_str == '*':
+            return month_str
+        return map( (lambda x: int(x)), month_str.split(',') )
+
+    #
+    def parseWeekDays(self, day_str):
+        if day_str == '*':
+            return day_str
+        return map( (lambda x: int(x)), day_str.split(',') )
+
 
 
 
