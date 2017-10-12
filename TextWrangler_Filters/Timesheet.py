cdf033d3723ec94522eda326178aa9c5ac381667
SleepyJay <agostaj@d20green.com>
2017-10-12 10:09:29 -0400

    TWF-xxx fixed the extra 00s and a bit of polish

--- TextWrangler_Filters/Timesheet.py
+++ TextWrangler_Filters/Timesheet.py
@@ -23,28 +23,40 @@ for thing in sorted(fileinput.input()):
     # Do not keep 0 duration tasks around
     if not tot: continue
 
-    # Task for which to apply remaing time
+    # Task for which to apply remaining time
     rem = tix.pop()
 
     for t in tix:
         t.strip()
         if not t: continue
-        if not t in sheet: sheet[t] = 0
+
+        if not t in sheet:
+            sheet[t] = 0
 
         sheet[t] += .25
-        tot      -= .25
+        tot -= .25
 
-    if not rem in sheet: sheet[rem] = 0
+        if not tot: break
+
+    if not rem in sheet:
+        sheet[rem] = 0
     sheet[rem] += tot
 
+
+#
+def prettier(tm):
+    return ':'.join(str(tm).split(':')[:2])
+
+
+##
 total = 0
 tm_start = datetime.timedelta(hours=9)
-tm_end   = tm_start
+tm_end = tm_start
 
 for key in sheet:
     total += sheet[key]
     tm_end = tm_start + datetime.timedelta(hours=sheet[key])
-    print( "{}\t{}\t{}-{}".format(key, sheet[key], tm_start, tm_end) )
+    print("{}\t{}\t{}-{}".format(key, sheet[key], prettier(tm_start), prettier(tm_end)))
     tm_start = tm_end
 
 print("\ntotal: {}".format(total))
