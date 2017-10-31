a27b4825f8488dd2f30c4e14ce81b3c2e928ea54
SleepyJay <agostaj@d20green.com>
2017-10-30 20:57:21 -0400

    PS-xxx Clean up, everybody, everywhere

--- PuzzleSolver/run.py
+++ PuzzleSolver/run.py
@@ -2,23 +2,23 @@
 
 import sys
 from Puzzles.DiamondPuzzle import DiamondPuzzle
-from Puzzles.LightsOutPuzzle import LightsOutPuzzle
+#from Puzzles.LightsOutPuzzle import LightsOutPuzzle
 
 action = "all"
 if len(sys.argv) > 1:
-	action = (sys.argv[1]).lower()
+    action = (sys.argv[1]).lower()
 
 if action == "diamond" or action == "all":
-	dp = DiamondPuzzle()
-	solution = dp.solve()
-	print("Diamond Solution ({}): {} ".format(dp.iterations, solution))
+    dp = DiamondPuzzle()
+    solution = dp.solve()
+    print("Diamond Solution ({}): {} ".format(dp.iterations, solution))
 
 if action == "lightsOut" or action == "all":
-	print("LightsOutPuzzle: not currently fully implemented");
-	#lo = LightsOutPuzzle()
-	#solution = lo.solve()
-	#print("LightsOut Solution ({}): {} ".format(lo.iterations, solution));
+    print("LightsOutPuzzle: not currently fully implemented");
+    #lo = LightsOutPuzzle()
+    #solution = lo.solve()
+    #print("LightsOut Solution ({}): {} ".format(lo.iterations, solution));
 
 
 
-	
+
