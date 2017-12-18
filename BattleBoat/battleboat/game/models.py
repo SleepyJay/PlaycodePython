114775b817176bb5c55207cac53a349391a1bf11
SleepyJay <crashb@d20green.com>
2017-12-17 19:23:50 -0500

    BB-xxx Removed BattleBoat

--- BattleBoat/battleboat/game/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Game(models.Model):
-    player_one_id = models.ForeignKey(Player)
-    player_two_id = models.ForeignKey(Player)
-    board_one_state = models.CharField(max_length=300)
-    board_two_state = models.CharField(max_length=300)
-
-
--- BattleBoat/battleboat/static/js/game.js
+++
@@ -1,2 +0,0 @@
-
-console.debug("Game.js Loaded");
